c47ab31f7b20db63355ca62edf2bc01ed38e6469
Nikola <nikola_m95@hotmail.com>
2018-08-21 19:38:16 +0200

    Created comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   
   resources :roles
+  
   root to: 'posts#index'
   
-  resources :posts do
-    resources :comments
-  end
+  resources :posts
+
+  resources :comments
+
   resources :categories
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
